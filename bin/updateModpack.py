2891b917a7589853224559cc182cd3529249ba8a
Cory J Reid <cjr@coryjreid.com>
2022-07-28 15:34:26 -0600

    cleanup python script formatting

--- bin/updateModpack.py
+++ bin/updateModpack.py
@@ -7,56 +7,53 @@ import subprocess
 
 
 def main():
-    managedFolders = ["config", "defaultconfigs",
-                      "kubejs", "resourcepacks", "scripts"]
+    managed_folders = ["config", "defaultconfigs", "kubejs", "resourcepacks", "scripts"]
     pwd = os.getcwd()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("curseManagedInstance",
-                        help="path to the CurseForge Launcher managed Minecraft instance")
-    parser.add_argument(
-        "versionNumber", help="new version number of the modpack")
+    parser.add_argument("curseManagedInstance", help="path to the CurseForge Launcher managed Minecraft instance")
+    parser.add_argument("versionNumber", help="new version number of the modpack")
     args = parser.parse_args()
 
-    mcInstanceFolder = args.curseManagedInstance
+    mc_instance_folder = args.curseManagedInstance
     version = args.versionNumber
 
     #########################
     # LOAD DATA
     #########################
-    sourceMods = {}
-    with open(os.path.join(mcInstanceFolder, "minecraftinstance.json")) as m:
-        mciData = json.load(m)
-        for mod in mciData["installedAddons"]:
+    source_mods = {}
+    with open(os.path.join(mc_instance_folder, "minecraftinstance.json")) as m:
+        mci_data = json.load(m)
+        for mod in mci_data["installedAddons"]:
             print(
                 f"Loading data for source mod '{mod['installedFile']['id']}'")
-            sourceMods[mod["addonID"]] = mod["installedFile"]["id"]
+            source_mods[mod["addonID"]] = mod["installedFile"]["id"]
 
-    packMods = {}
+    pack_mods = {}
     for filename in os.listdir(os.path.join(pwd, "mods")):
         with open(os.path.join(pwd, "mods", filename), "r") as f:
             mod = toml.load(f)
             if "update" in mod:
                 print(f"Loading data for pack mod '{mod['update']['curseforge']['project-id']}'")
-                packMods[mod["update"]["curseforge"]["project-id"]] = {
+                pack_mods[mod["update"]["curseforge"]["project-id"]] = {
                     "fileId": mod["update"]["curseforge"]["file-id"],
-                    "packSlug": filename.replace(".toml", "")
+                    "packSlug": filename.replace(".pw.toml", "")
                 }
 
     #########################
     # DO PACK UPDATES
     #########################
-    for key in (set(sourceMods.keys()) - set(packMods.keys())):
+    for key in (set(source_mods.keys()) - set(pack_mods.keys())):
         # install
-        print(f"Installing pack mod {key}, file {sourceMods[key]}")
+        print(f"Installing pack mod {key}, file {source_mods[key]}")
         subprocess.run([os.path.join(pwd, "bin", "packwiz.exe"), "curseforge",
-                       "install", "--addon-id", str(key), "--file-id", str(sourceMods[key])])
+                        "install", "--addon-id", str(key), "--file-id", str(source_mods[key])])
 
-    for modId in packMods:
-        modData = packMods[modId]
-        if modId in sourceMods:
-            slug = modData['packSlug'].replace(".pw", "")
-            if sourceMods[modId] != modData["fileId"]:
+    for modId in pack_mods:
+        mod_data = pack_mods[modId]
+        if modId in source_mods:
+            slug = mod_data['packSlug']
+            if source_mods[modId] != mod_data["fileId"]:
                 # update
                 print(f"Updating pack mod {slug}")
                 subprocess.run(
@@ -68,16 +65,16 @@ def main():
                 [os.path.join(pwd, "bin", "packwiz.exe"), "remove", slug])
 
     # nuke all managed folders in repo
-    for dir in managedFolders:
-        path = os.path.join(pwd, dir)
+    for folder in managed_folders:
+        path = os.path.join(pwd, folder)
         if os.path.isdir(path):
             print(f"Deleting '{path}'")
             shutil.rmtree(path)
 
     # copy managed folder changes from Minecraft instance folder
-    for dir in managedFolders:
-        src = os.path.join(mcInstanceFolder, dir)
-        dst = os.path.join(pwd, dir)
+    for folder in managed_folders:
+        src = os.path.join(mc_instance_folder, folder)
+        dst = os.path.join(pwd, folder)
         print(f"Copying '{src}' to '{dst}'")
         shutil.copytree(src, dst)
 
@@ -85,25 +82,25 @@ def main():
     # UPDATE BCC
     #########################
     # load data
-    bccFile = os.path.join(pwd, "config", "bcc-common.toml")
-    print(f"Loading BCC data from '{bccFile}'")
-    bccData = toml.load(bccFile)
+    bcc_file = os.path.join(pwd, "config", "bcc-common.toml")
+    print(f"Loading BCC data from '{bcc_file}'")
+    bcc_data = toml.load(bcc_file)
     print(f"Success")
 
     # set default BCC values
-    bccData['general']['modpackName'] = "Logicraft_"
-    bccData['general']['modpackProjectID'] = 323471
-    bccData['general']['useMetadata'] = False
+    bcc_data['general']['modpackName'] = "Logicraft_"
+    bcc_data['general']['modpackProjectID'] = 323471
+    bcc_data['general']['useMetadata'] = False
 
     # update version
     print(f"Setting BCC pack version to {version}")
-    bccData['general']['modpackVersion'] = version
+    bcc_data['general']['modpackVersion'] = version
     print("Success")
 
     # write changes to BCC file
-    with open(bccFile, 'w') as outfile:
+    with open(bcc_file, 'w') as outfile:
         print(f"Writing BCC file")
-        toml.dump(bccData, outfile)
+        toml.dump(bcc_data, outfile)
         print("Success")
 
     #########################
@@ -124,10 +121,10 @@ def main():
     #########################
     with open(os.path.join(pwd, "VERSION"), 'w') as f:
         f.write(version)
-    
+
     print("Refreshing packwiz index")
     subprocess.run(
-                [os.path.join(pwd, "bin", "packwiz.exe"), "refresh"])
+        [os.path.join(pwd, "bin", "packwiz.exe"), "refresh"])
     print("Success")
 
     print("Pack update complete")
