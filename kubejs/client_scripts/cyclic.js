f30b9fba89a5b5daccf2810cc71b47fe1fc78306
Cory J Reid <11530109+coryjreid@users.noreply.github.com>
2023-08-23 19:14:06 -0600

    Removed a bunch of crap from Cyclic that was not needed

---
+++ kubejs/client_scripts/cyclic.js
@@ -0,0 +1,25 @@
+JEIEvents.hideFluids(event => {
+    //console.log('Hello! The JEI hide fluids event has fired!')
+    let fluids = [
+        'cyclic:xpjuice',
+        'cyclic:slime',
+        'cyclic:biomass',
+        'cyclic:honey',
+        'cyclic:magma',
+        'cyclic:wax'
+    ]
+    fluids.forEach(fluid => event.hide(fluid))
+})
+JEIEvents.hideItems(event => {
+    event.hide(/^cyclic:(?:(?!(apple_ender|apple_honey|apple_bone|apple_prismarine|apple_lapis|apple_chocolate|charm_creeper|charm_stone|charm_antipotion|charm_boostpotion|charm_crit|charm_invisible|charm_magicdefense|charm_starvation|charm_venom|charm_water|charm_speed|charm_luck|charm_attack_speed|charm_xp_speed|charm_xp_blocker|gem_obsidian|gem_amber|flint_block|magic_net|flippers|charm_void|charm_antidote|charm_fire|charm_wither|charm_ultimate|heart_empty|torch_launcher|charm_torch|charm_torch_cave|eye_teleport|anvil|experience_food|sleeping_mat|fireball)$)).+$/)
+    event.hide(Item.of('minecraft:potion', '{Potion:"cyclic:*"}'))
+    event.hide(Item.of('minecraft:splash_potion', '{Potion:"cyclic:*"}'))
+    event.hide(Item.of('minecraft:lingering_potion', '{Potion:"cyclic:*"}'))
+    event.hide(Item.of('minecraft:tipped_arrow', '{Potion:"cyclic:*"}'))
+})
+JEIEvents.removeCategories(event => {
+    //console.log(event.getCategoryIds())
+    event.remove("cyclic:crusher")
+    event.remove("cyclic:packager")
+    event.remove("cyclic:solidifier")
+})
---
+++ kubejs/server_scripts/cyclic.js
@@ -0,0 +1,71 @@
+ServerEvents.recipes(event => {
+    event.remove({
+        mod: 'cyclic',
+        not: [
+            { output: 'cyclic:apple_ender' },
+            { output: 'cyclic:apple_honey' },
+            { output: 'cyclic:apple_bone' },
+            { output: 'cyclic:apple_prismarine' },
+            { output: 'cyclic:apple_lapis' },
+            { output: 'cyclic:apple_chocolate' },
+            { output: 'cyclic:charm_creeper' },
+            { output: 'cyclic:charm_stone' },
+            { output: 'cyclic:charm_antipotion' },
+            { output: 'cyclic:charm_boostpotion' },
+            { output: 'cyclic:charm_crit' },
+            { output: 'cyclic:charm_invisible' },
+            { output: 'cyclic:charm_magicdefense' },
+            { output: 'cyclic:charm_starvation' },
+            { output: 'cyclic:charm_venom' },
+            { output: 'cyclic:charm_water' },
+            { output: 'cyclic:charm_speed' },
+            { output: 'cyclic:charm_luck' },
+            { output: 'cyclic:charm_attack_speed' },
+            { output: 'cyclic:charm_xp_speed' },
+            { output: 'cyclic:charm_xp_blocker' },
+            { output: 'cyclic:gem_obsidian' },
+            { output: 'cyclic:gem_amber' },
+            { output: 'cyclic:flint_block' },
+            { output: 'cyclic:magic_net' },
+            { output: 'cyclic:flippers' },
+            { output: 'cyclic:charm_void' },
+            { output: 'cyclic:charm_antidote' },
+            { output: 'cyclic:charm_fire' },
+            { output: 'cyclic:charm_wither' },
+            { output: 'cyclic:charm_ultimate' },
+            { output: 'cyclic:heart_empty' },
+            { output: 'cyclic:torch_launcher' },
+            { output: 'cyclic:charm_torch' },
+            { output: 'cyclic:charm_torch_cave' },
+            { output: 'cyclic:eye_teleport' },
+            { output: 'cyclic:anvil' },
+            { output: 'cyclic:experience_food' },
+            { output: 'cyclic:sleeping_mat' },
+            { output: 'cyclic:fireball' },
+        ]
+    })
+    event.shapeless(
+        Item.of('cyclic:experience_food', 1),
+        [ 
+          'minecraft:sugar',
+          'minecraft:sugar',
+          'minecraft:sugar'
+        ]
+    )
+    event.shapeless(
+        Item.of('cyclic:gem_amber', 2),
+        [
+            'minecraft:fire_charge',
+            'minecraft:redstone',
+            'minecraft:magma_block'
+        ]
+    )
+    event.shapeless(
+        Item.of('cyclic:gem_obsidian', 2),
+        [
+            'minecraft:emerald',
+            'cyclic:gem_amber',
+            'minecraft:obsidian'
+        ]
+    )
+})
