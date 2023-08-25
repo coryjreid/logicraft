0ff3308ccf6407d7735e8a63deac40c545702daf
Cory J Reid <11530109+coryjreid@users.noreply.github.com>
2023-08-24 21:44:32 -0600

    Added compatibility recipes for Mekanism Crusher & Thermal Pulverizer

---
+++ kubejs/client_scripts/jei_hide_items.js
@@ -0,0 +1,8 @@
+// Big Reactors (Extreme Reactors 2)
+JEIEvents.hideItems(event => {
+    event.hide(Item.of('bigreactors:blutonium_dust'))
+    event.hide(Item.of('bigreactors:magentite_dust'))
+    event.hide(Item.of('bigreactors:ludicrite_dust'))
+    event.hide(Item.of('bigreactors:ridiculite_dust'))
+    event.hide(Item.of('bigreactors:inanite_dust'))
+})
